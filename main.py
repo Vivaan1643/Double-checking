12d7b5c2991873ccfed17341cfbeb43c799968fa
Vivaan1643 <vivaan.k.urs@gmail.com>
2024-12-17 16:34:31 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+a=int(input("Enter number"))
+if a>50:
+    print("Number is greater than 50")
+    if a%2==0:
+        print ("number is even")
+    else:
+        print("number is odd")
+else:
+    print("number is less than 50")            
